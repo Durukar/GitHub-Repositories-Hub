2db1908834d0e387e20ecd8c406d33d0741072b6
Durukar <nightdavila@gmail.com>
2024-08-30 08:05:08 -0300

    Dark mode implementado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { PersonStanding, Search } from 'lucide-react'
 import React, { useState } from 'react'
 
+import { ThemeProvider } from './components/theme/theme-provider'
+import { ThemeToggle } from './components/theme/theme-toggle'
 import { Button } from './components/ui/button'
 import { Checkbox } from './components/ui/checkbox'
 import {
@@ -141,87 +143,94 @@ export function App() {
   // })
 
   return (
-    <div className="p-6 max-w-5xl mx-auto space-y-2">
-      <h1 className="text-3xl font-bold">GitHub Repositories HUB</h1>
-      <div className="flex items-center justify-between">
-        <form className="flex items-center gap-2" onSubmit={handleSubmit}>
-          <Input
-            name="user"
-            placeholder="Usuario GitHub"
-            className="w-auto"
-            value={user}
-            onChange={handleUserGitHub}
-          />
-          <Input
-            name="token"
-            placeholder="Token do github"
-            className="w-auto"
-            value={token}
-            onChange={handleTokenGitHub}
-          />
+    <ThemeProvider storageKey="gitHubRepositories" defaultTheme="dark">
+      <div className="p-6 max-w-5xl mx-auto space-y-2">
+        <div className="ml-auto flex items-center gap-2">
+          <h1 className="text-3xl font-bold mr-auto">
+            GitHub Repositories HUB
+          </h1>
+          <ThemeToggle />
+        </div>
+        <div className="flex items-center justify-between">
+          <form className="flex items-center gap-2" onSubmit={handleSubmit}>
+            <Input
+              name="user"
+              placeholder="Usuario GitHub"
+              className="w-auto"
+              value={user}
+              onChange={handleUserGitHub}
+            />
+            <Input
+              name="token"
+              placeholder="Token do github"
+              className="w-auto"
+              value={token}
+              onChange={handleTokenGitHub}
+            />
 
-          <Button type="submit" onClick={fetchGitRepos} disabled={loading}>
-            <Search className="w-4 h-4 mr-2" />
-            Localizar repositorios
-          </Button>
-        </form>
-
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="link">
-              <PersonStanding className="w-4 h-4 mr-2" />
-              Dev
+            <Button type="submit" onClick={fetchGitRepos} disabled={loading}>
+              <Search className="w-4 h-4 mr-2" />
+              Localizar repositorios
             </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogTitle>Infos do criador</DialogTitle>
-            <DialogDescription>Lucas D</DialogDescription>
-          </DialogContent>
-        </Dialog>
-      </div>
-      <div className="border rounded-lg p-2 h-full">
-        <Table>
-          <TableHeader>
-            <TableHead>Repo ID</TableHead>
-            <TableHead>Repo Name</TableHead>
-            <TableHead>Repo URL</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead className="m">Mudar Status</TableHead>
-          </TableHeader>
-          <TableBody>
-            {repository?.map((c, i) => {
-              return (
-                <TableRow key={i}>
-                  <TableCell>{c.id}</TableCell>
-                  <TableCell>{c.name}</TableCell>
-                  <TableCell>{c.url}</TableCell>
-                  <TableCell>{c.private ? 'Privado' : 'Publico'}</TableCell>
-                  <TableCell
-                    className="flex gap-5 justify-between items-center"
-                    key={i}
-                  >
-                    <Checkbox
-                      disabled={loading}
-                      onClick={() => handleStatusChange(c.id)}
-                      checked={checkBoxSelected[c.id] || false}
-                    />
-                    <Button
-                      disabled={!checkBoxSelected[c.id] || loading}
-                      onClick={() =>
-                        handleChangeStatus(user, c.name, !c.private)
-                      }
-                      className="bg-blue-500 text-white px-4 py-2 rounded"
+          </form>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="link">
+                <PersonStanding className="w-4 h-4 mr-2" />
+                Dev
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>Infos do criador</DialogTitle>
+              <DialogDescription>Lucas D</DialogDescription>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <div className="border rounded-lg p-2 h-full">
+          <Table>
+            <TableHeader>
+              <TableHead>Repo ID</TableHead>
+              <TableHead>Repo Name</TableHead>
+              <TableHead>Repo URL</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead className="m">Mudar Status</TableHead>
+            </TableHeader>
+            <TableBody>
+              {repository?.map((c, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell>{c.id}</TableCell>
+                    <TableCell>{c.name}</TableCell>
+                    <TableCell>{c.url}</TableCell>
+                    <TableCell>{c.private ? 'Privado' : 'Publico'}</TableCell>
+                    <TableCell
+                      className="flex gap-5 justify-between items-center"
+                      key={i}
                     >
-                      Aplicar
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
+                      <Checkbox
+                        disabled={loading}
+                        onClick={() => handleStatusChange(c.id)}
+                        checked={checkBoxSelected[c.id] || false}
+                      />
+                      <Button
+                        disabled={!checkBoxSelected[c.id] || loading}
+                        onClick={() =>
+                          handleChangeStatus(user, c.name, !c.private)
+                        }
+                        className="bg-blue-500 text-white px-4 py-2 rounded"
+                      >
+                        Aplicar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
